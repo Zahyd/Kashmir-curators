c30dd685142a578ef2ba4f0fea6862289c4b9474
Zahyd <zahidreyaz44123@gmail.com>
2026-06-27 22:25:35 +0530

    feat(server): increase express request payload limits to 50mb to allow base64 images inside itinerary saves

--- server/src/index.ts
+++ server/src/index.ts
@@ -104,7 +104,8 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Socket.io Connection
 io.on('connection', (socket) => {
